885f19f7e5dc95cc7c2486917e01b3414886295c
Eunice Sarah Siregar <13521013@mahasiswa.itb.ac.id>
2023-09-27 19:48:15 +0700

    fix folder

--- src/frontend/public/lapor.php
+++ src/frontend/public/lapor.php
@@ -22,11 +22,11 @@
             </label>
             <label class="logo"><span class="say">Say</span><span class="it">It</span></label>
             <ul>
-                <li <?php if($page == "Home") echo "class='active'"; ?>><a href="../../../index.php">Home</a></li>
+                <li <?php if($page == "Home") echo "class='active'"; ?>><a href="../../../../index.php">Home</a></li>
                 <li <?php if($page == "Artikel") echo "class='active'"; ?>><a href="artikel.php">Artikel</a></li>
                 <li <?php if($page == "Ruang Diskusi") echo "class='active'"; ?>><a href="ruangdiskusi.php">Ruang Diskusi</a></li>
                 <li <?php if($page == "Lapor") echo "class='active'"; ?>><a href="lapor.php">Lapor</a></li>
-                <li><a href="#">Login</a></li>
+                <li <?php if($page == "SignUp") echo "class='active'"; ?>><a href="signup.php">Sign Up</a></li>
             </ul>
         </nav>
         <section class="container">
--- src/frontend/public/signup.php
+++ src/frontend/public/signup.php
@@ -4,8 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
+    <link rel="stylesheet" href="assets/css/style.css">        	
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
 <body>
+
+<nav class="navbar">
+            <input type="checkbox" id="nav-check">
+            <label for="nav-check" class="check-button">
+                <i class="fa fa-bars"></i>
+            </label>
+            <label class="logo"><span class="say">Say</span><span class="it">It</span></label>
+            <ul>
+                <li <?php if($page == "Home") echo "class='active'"; ?>><a href="../../../../index.php">Home</a></li>
+                <li <?php if($page == "Artikel") echo "class='active'"; ?>><a href="artikel.php">Artikel</a></li>
+                <li <?php if($page == "Ruang Diskusi") echo "class='active'"; ?>><a href="ruangdiskusi.php">Ruang Diskusi</a></li>
+                <li <?php if($page == "Lapor") echo "class='active'"; ?>><a href="lapor.php">Lapor</a></li>
+                <li <?php if($page == "SignUp") echo "class='active'"; ?>><a href="signup.php">Sign Up</a></li>
+            </ul>
+        </nav>
+        <section></section>
+        <div class="signupbg">
+    <div>
     <h1>Sign Up</h1>
     <form action="../backend/validate-signup.php" method="post">
         <div>
@@ -38,5 +59,7 @@
         
         <button type="submit" id="register">Register</button>
     </form>
+</div>
+</div>
 </body>
 </html>
