0dac708c430caf3c419c0d18bc1e5249f3044804
goodgirlwannabe <13521006@mahasiswa.itb.ac.id>
2023-09-29 21:26:25 +0700

    fix: styles

--- src/app/components/lapor.php
+++ src/app/components/lapor.php
@@ -17,28 +17,8 @@
     </head>
     <body>
         <!-- Navbar -->
-        <nav class="navbar">
-            <input type="checkbox" id="nav-check">
-            <label for="nav-check" class="check-button">
-                <i class="fa fa-bars"></i>
-            </label>
-            <label class="logo"><span class="say">Say</span><span class="it">It</span></label>
-            <ul>
-                <li <?php if($page == "Home") echo "class='active'"; ?>><a href="../../../../index.php">Home</a></li>
-                <li <?php if($page == "Artikel") echo "class='active'"; ?>><a href="artikel.php">Artikel</a></li>
-                <li <?php if($page == "Ruang Diskusi") echo "class='active'"; ?>><a href="ruangdiskusi.php">Ruang Diskusi</a></li>
-                <li <?php if($page == "Lapor") echo "class='active'"; ?>><a href="lapor.php">Lapor</a></li>
-                <?php
-                if (isset($_SESSION["user_email"])) {
-                    echo "<li><a href='../../../../index.php'>Home</a></li>";
-                    echo "<li><a href='logout.php'>Log Out</a></li>";
-                }
-                else{
-                    echo "<li><a href='signup.php'>Sign Up</a></li>";
-                }
-                ?>
-            </ul>
-        </nav>
+        <?php include (dirname(__DIR__)) . "/components/navbar.php" ?>
+        
         <section class="container">
             <div class="form-lapor">
                 <h3 class="judul">Form Laporan Pelecehan Seksual</h3>
