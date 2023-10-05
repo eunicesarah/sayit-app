b28a0f7897e42a0dc05314065c5e9be92ee28606
goodgirlwannabe <13521006@mahasiswa.itb.ac.id>
2023-10-05 21:27:02 +0700

    feat: favicon

--- src/app/components/lapor/index.php
+++ src/app/components/lapor/index.php
@@ -13,6 +13,7 @@ $page = "Lapor";
     <meta charset="UTF-8">
     <title>SayIt</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/src/public/img/gimmick.png" type="image/x-icon">
     <link rel="stylesheet" href="/src/public/css/lapor.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
--- src/app/components/login/index.php
+++ src/app/components/login/index.php
@@ -46,6 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SayIt</title>
+    <link rel="icon" href="/src/public/img/gimmick.png" type="image/x-icon">
     <link rel="stylesheet" href="/src/public/css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
